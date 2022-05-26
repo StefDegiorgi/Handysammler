8c9b2959cd712ea1b7f5888241021a7355715ed3
Stefano Degiorgi <degiorgis@bzz.ch>
2022-05-26 07:58:04 +0200

    changin files for milestone 1

--- src/main/java/ch/bzz/handy/model/Handymodell.java
+++ src/main/java/ch/bzz/handy/model/Handymodell.java
@@ -1,14 +1,41 @@
 package ch.bzz.handy.model;
 
+import ch.bzz.handy.data.DataHandler;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * Ein Handymodell der Handymarke
  */
 
 public class Handymodell {
+
+    @JsonIgnore
+    private Handymarke handymarke;
+
     private String handymodellID;
     private String handymodellName;
     private String erscheinungsjahr;
     private double akkulaufzeit;
+
+    public String getHandymarkeID() {
+        if (getHandymarke()== null) return null;
+        return getHandymarke().getHandymarkeID();
+    }
+
+    public void setHandymarkeID(String handymarkeID) {
+        setHandymarke(new Handymarke());
+        Handymarke handymarke = DataHandler.readHandymarkeByID(handymarkeID);
+        getHandymarke().setHandymarkeID(handymarkeID);
+        getHandymarke().setHandymarkeName(handymarke.getHandymarkeName());
+        getHandymarke().setHerkunftsland((handymarke.getHerkunftsland()));
+        getHandymarke().setGruendungsDatum(handymarke.getGruendungsDatum());
+    }
+    public Handymarke getHandymarke(){
+        return handymarke;
+    }
+    public void setHandymarke(Handymarke handymarke) {
+        this.handymarke = handymarke;
+    }
 
     public String getHandymodellID() {
         return handymodellID;
